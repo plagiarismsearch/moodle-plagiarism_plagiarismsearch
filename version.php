c8fa279d8c79121d9a19f137b9b3f30dc1433d6b
Alex Crosby <developer@plagiarismsearch.com>
2018-03-09 15:13:03 +0200

    New version

--- version.php
+++ version.php
@@ -20,9 +20,9 @@ if (!isset($plugin)) {
     $plugin = new stdClass();
 }
 
-$plugin->version = 2018021900; // YYYYMMDDVV.
+$plugin->version = 2018030900; // YYYYMMDDVV.
 $plugin->requires = 2014051200; // Requires Moodle 2.7.
 $plugin->cron = 60;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'plagiarism_plagiarismsearch';
-$plugin->release = '1.1.7';
+$plugin->release = '1.1.8';
