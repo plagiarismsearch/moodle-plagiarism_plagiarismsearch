cb0f76374ca3132f95ee21534ab5e25425af2bf3
Alex Crosby <developer@plagiarismsearch.com>
2018-08-13 14:44:22 +0300

    Spanish translation

---
+++ lang/es/plagiarism_plagiarismsearch.php
@@ -0,0 +1,81 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package    plagiarism_plagiarismsearch
+ * @author     Alex Crosby developer@plagiarismsearch.com
+ * @copyright  @2018 PlagiarismSearch.com
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+$string['plagiarismsearch'] = 'PlagiarismSearch';
+$string['pluginname'] = 'Plugin de plagio de PlagiarismSearch';
+$string['auto_check'] = 'Autocheck';
+$string['manual_check'] = 'Manual check';
+$string['add_to_storage'] = 'Add to depósito institucional';
+$string['sources_type'] = 'Sources';
+$string['sources_doc_web_storage'] = 'Doc vs Web + Depósito';
+$string['sources_doc_web'] = 'Doc vs Web';
+$string['sources_doc_storage'] = 'Doc vs Depósito';
+$string['student_show_percentage'] = 'Mostrar los informes de originalidad a los estudiantes';
+$string['student_show_reports'] = '¿Permitir los estudiantes download .pdf reports';
+$string['student_submit'] = '¿Permitir los estudiantes submit papers';
+$string['student_resubmit'] = '¿Permitir los estudiantes volver a entregar papers';
+$string['student_resubmit_numbers'] = 'The number of Volver a entregar';
+$string['student_disclosure'] = 'Revelación de estudiantes';
+$string['student_disclosure_default'] = 'All files uploaded will be submitted to PlagiarismSearch.com detection service.';
+$string['student_error_nopermission'] = 'No nopermission';
+$string['api_url'] = 'URL de la API';
+$string['api_key'] = 'Api key';
+$string['api_user'] = 'Api user';
+$string['api_version'] = 'Api version';
+$string['filter_chars'] = 'Only latin characters';
+$string['filter_references'] = 'Excluir bibliografía';
+$string['filter_quotes'] = 'Excluir citas bibliográficas';
+$string['submit'] = 'Enviar a entregar a PlagiarismSearch';
+$string['resubmit'] = 'Volver a entregar de PlagiarismSearch';
+$string['processing'] = 'Procesamiento en curso';
+$string['unknown_error'] = 'Se ha producido un error desconocido';
+$string['pdf_report'] = 'Download .pdf informe';
+$string['link_title'] = 'PlagiarismSearch.com – advanced online plagiarism checker';
+$string['check_status'] = 'Check status';
+$string['temp_folder_not_exists'] = 'Temp folder does not exist';
+$string['server_connection_error'] = 'Problem connecting to PlagiarismSearch server';
+$string['submit_ok'] = 'Document \'{$a}\' submitted to PlagiarismSearch';
+$string['submit_error'] = 'Document \'{$a}\' not submitted';
+$string['submit_onlinetext_ok'] = 'Online text submitted to PlagiarismSearch';
+$string['submit_onlinetext_error'] = 'Online text not submitted';
+$string['status_ok'] = 'Document status';
+$string['status_error'] = 'Error document status';
+$string['empty_parameter'] = 'Empty \'{$a}\' parameter';
+$string['report_not_found'] = 'Informe not found';
+$string['no_cmid_or_id'] = 'No cmid or id';
+$string['report'] = 'Informe';
+$string['save'] = 'Guardar';
+$string['settings_error'] = 'There was an error whilst updating the PlagiarismSearch settings';
+$string['settings_error_server'] = 'PlagiarismSearch settings api error';
+$string['settings_saved'] = 'Opciones de PlagiarismSearch guardada';
+// $string['settings_cancelled'] = 'PlagiarismSearch settings have been canceled';
+// $string['settings_saved'] = 'Opciones de PlagiarismSearch guardada';
+$string['submit'] = 'Submit to PlagiarismSearch';
+$string['use'] = 'Habilitar PlagiarismSearch';
+$string['yellow'] = 'Plagiarism level yellow starts at';
+$string['red'] = 'Plagiarism level red starts at';
+$string['text_plain'] = 'PlagiarismSearch.com – advanced online  plagiarism checker. <br/> PlagiarismSearch.com is a leading plagiarism checking website that  will provide you with an accurate report during a short timeframe. <br/> Find out about how to register <a href="https://plagiarismsearch.com/account/signup.html">here</a> and ask us for a free trial <a href="mailto:support@plagiarismsearch.com">support@plagiarismsearch.com</a><br/><br/>';
+
+$string['plagiarismsearch:viewlinks'] = 'Ability to view links for plagiarism results';
+$string['plagiarismsearch:submitlinks'] = 'Ability to submit links to PlagiarismSearch.com';
+$string['plagiarismsearch:statuslinks'] = 'Ability to check report status';
+$string['plagiarismsearch:isstudent'] = 'Allow only los estudiantes';
