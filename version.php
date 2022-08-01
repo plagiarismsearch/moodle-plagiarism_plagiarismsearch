5fea9550de61c2316088a9f15c7f90cfbaba6bcf
kozulov <kozulov@gmail.com>
2022-08-01 18:29:28 +0300

    Version++

--- version.php
+++ version.php
@@ -19,9 +19,9 @@ defined('MOODLE_INTERNAL') || die();
 if (!isset($plugin)) {
     $plugin = new \stdClass();
 }
-$plugin->version = 2021051200; // YYYYMMDDVV.
+$plugin->version = 2022080100; // YYYYMMDDVV.
 $plugin->requires = 2014051200; // Requires Moodle 2.7.
 $plugin->cron = 60;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'plagiarism_plagiarismsearch';
-$plugin->release = '1.1.14';
+$plugin->release = '1.1.15';
